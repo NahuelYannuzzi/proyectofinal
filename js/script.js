80c23240981df7fa545cd532e1b03425df21485d
proyecto <yannuzzinahuel113@hotmail.com>
2023-06-03 02:12:02 -0300

    Actualizacion de proyecto

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-//----------------------------Comentarios JS
+//-------------------------------------------------------------------Contacto
+
+function enviar(){
+    alert("Nos pondremos en contacto con usted en breve")
+}
+
+//-------------------------------------------------------------------Productos
+
+function stock(){
+    alert("No hay stock disponible, Intentelo mas tarde")
